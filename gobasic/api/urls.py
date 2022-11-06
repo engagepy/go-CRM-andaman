b38199e399b742874e37b3fdcad5cf31861f7154
zora89 <zorawar.purohit@gmail.com>
2022-11-06 09:06:31 +0530

    API feature for Hotel and Activity added, be careful of where this data goes

--- gobasic/api/urls.py
+++ gobasic/api/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
 from rest_framework import routers
-from .views import  GroupViewSet, UserViewSet, TripViewSet
+from .views import  GroupViewSet, UserViewSet, TripViewSet, HotelViewSet, ActivityViewSet
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
 router.register(r'groups', GroupViewSet)
 router.register(r'trips', TripViewSet)
+router.register(r'hotels', HotelViewSet)
+router.register(r'activitys', ActivityViewSet)
 
 urlpatterns = [
     path('api/', include(router.urls)),
--- gobasic/api/views.py
+++ gobasic/api/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets
 from rest_framework import permissions
-from gobasic.serializers import UserSerializer, GroupSerializer, TripSerializer
+from .serializers import UserSerializer, GroupSerializer, TripSerializer, HotelSerializer, ActivitySerializer
 from django.contrib.auth.models import User, Group
 from gobasic.models import  Trip, Customer, Hotel, Activity
 
@@ -16,6 +16,22 @@ class TripViewSet(viewsets.ModelViewSet):
     """
     queryset = Trip.objects.all().order_by('end_date')
     serializer_class = TripSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class HotelViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = Hotel.objects.all()
+    serializer_class = HotelSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class ActivityViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = Activity.objects.all()
+    serializer_class = ActivitySerializer
     permission_classes = [permissions.IsAuthenticated]
 
 class UserViewSet(viewsets.ModelViewSet):
