73f8d5b1e11f7647726f3d3bcaa9279ba779ef5e
zora89 <zorawar.purohit@gmail.com>
2022-12-25 05:13:36 +0530

    signals for trip total working fully

--- gobasic/signals.py
+++ gobasic/signals.py
@@ -2,24 +2,25 @@ from django.db.models.signals import pre_save, post_save, m2m_changed
 from django.dispatch import receiver
 from .models import Trip
 
-@receiver(post_save, sender=Trip)
-def trip_final_cal(sender, instance, **kwargs):
+
+
+# @receiver(pre_save, sender=Trip)
+# def trip_final_cal(sender, instance, update_fields='activity_cost', **kwargs):
+#     total = 0
+#     activity_selected = instance.activities.all()
+#     for i in activity_selected:
+#         print('via pre_save '+ str(i.net_cost))
+#         total += i.net_cost
+#     print(total)
+#     instance.activity_cost=total
+
+@receiver(m2m_changed, sender=Trip.activities.through)
+def activity_final_cal(sender, instance, action,model,pk_set, *args, **kwargs):
     total = 0
     activity_selected = instance.activities.all()
     for i in activity_selected:
-        print('via post_save '+ str(i.net_cost))
+        print('via m2m ' + str(i))
         total += i.net_cost
     print(total)
-    Trip.activity_cost = total
-    return Trip.activity_cost
-
-# @receiver(m2m_changed, sender=Trip)
-# def trip_final_cal(sender, instance, **kwargs):
-#     total = 0
-#     activity_selected = sender.activity.all()
-#     for i in activity_selected:
-#         print('via m2m ' + str(i))
-#         total += i.net_cost
-#     print(total)
-#     Trip.activity_cost = total
-#     return Trip.activity_cost
+    instance.activity_cost = total
+    instance.save()
