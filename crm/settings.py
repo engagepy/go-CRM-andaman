6fa5882c6b40be177c0f5a13f65a046149135d08
zora89 <zorawar.purohit@gmail.com>
2023-02-26 03:48:41 +0530

    added pages site

--- crm/settings.py
+++ crm/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ['APP_PASSWORD']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['astratechz.pagekite.me','127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['crm']
 
 
 # Application definition
