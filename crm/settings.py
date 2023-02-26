7300c642f7a3c75e0b581edfa55b400327b73ad6
engagepy <42845567+engagepy@users.noreply.github.com>
2023-02-26 23:29:37 +0530

    Profile Model Added (#12)

--- crm/settings.py
+++ crm/settings.py
@@ -131,7 +131,7 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
-AUTH_USER_MODEL = 'gobasic.User'
+#AUTH_USER_MODEL = 'gobasic.User'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOW_ALL_ORIGINS = True
--- gobasic/admin.py
+++ gobasic/admin.py
@@ -1,8 +1,22 @@
 from django.contrib import admin
-from .models import Customer, Hotel, Activity, Trip, User, Locations
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+from .models import Customer, Hotel, Activity, Trip, User, Locations, Profile
+
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'profile'
+
+class UserAdmin(BaseUserAdmin):
+    inlines = (ProfileInline, )
+
+admin.site.unregister(User)
+
 
 # Register your models here.
-admin.site.register(User)
+admin.site.register(User, UserAdmin)
 admin.site.register(Customer)
 admin.site.register(Trip)
 admin.site.register(Activity)
