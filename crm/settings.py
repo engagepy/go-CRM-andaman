a27bc0f1bbcf5aa5f95c56e03a34fe20f72e2fa3
zora89 <zorawar.purohit@gmail.com>
2023-04-16 03:07:58 +0530

    production db name changed

--- crm/settings.py
+++ crm/settings.py
@@ -87,7 +87,7 @@ WSGI_APPLICATION = 'crm.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mydb',
+        'NAME': '-',
         'USER': 'postgres',
         'PASSWORD': os.environ['PASSWORD'],
         'HOST': os.environ['DB_ENDPOINT'],
